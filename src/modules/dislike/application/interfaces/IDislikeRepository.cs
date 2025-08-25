1d84750b3335646094904ff9f97efcca4f07763b
servorx <191423123+servorx@users.noreply.github.com>
2025-08-25 16:36:16 -0500

    feat: :sparkles: dislike finished, I think

--- src/modules/dislike/application/interfaces/IDislikeRepository.cs
+++ src/modules/dislike/application/interfaces/IDislikeRepository.cs
@@ -2,9 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using campuslove_angel_daniela.src.modules.dislike.domain.models;
 
 namespace campuslove_angel_daniela.src.modules.dislike.application.interfaces;
 public interface IDislikeRepository
 {
-    
+    Task<Dislike> CrearDislikeAsync(Dislike dislike);
+    Task<Dislike?> ObtenerPorIdAsync(int id);
+    Task<IEnumerable<Dislike>> ObtenerTodosAsync();
+    Task<IEnumerable<Dislike>> ObtenerPorEmisorAsync(int idEmisor);
+    Task<IEnumerable<Dislike>> ObtenerPorReceptorAsync(int idReceptor);
+    Task EliminarAsync(int id);
 }
--- src/modules/dislike/infrastructure/repositories/DislikeRepository.cs
+++ src/modules/dislike/infrastructure/repositories/DislikeRepository.cs
@@ -2,9 +2,65 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using campuslove_angel_daniela.src.modules.dislike.domain.models;
+using campuslove_angel_daniela.src.shared.context;
+using Microsoft.EntityFrameworkCore;
 
 namespace campuslove_angel_daniela.src.modules.dislike.infrastructure.repositories;
 public class DislikeRepository
 {
+    private readonly AppDbContext _context;
 
+    public DislikeRepository(AppDbContext context) =>_context = context;    
+
+    public async Task<Dislike> CrearDislikeAsync(Dislike dislike)
+    {
+        _context.Dislikes.Add(dislike);
+        await _context.SaveChangesAsync();
+        return dislike;
+    }
+
+    public async Task<Dislike?> ObtenerPorIdAsync(int id)
+    {
+        return await _context.Dislikes
+            .Include(d => d.Emisor)
+            .Include(d => d.Receptor)
+            .FirstOrDefaultAsync(d => d.Id == id);
+    }
+
+    public async Task<IEnumerable<Dislike>> ObtenerTodosAsync()
+    {
+        return await _context.Dislikes
+            .Include(d => d.Emisor)
+            .Include(d => d.Receptor)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Dislike>> ObtenerPorEmisorAsync(int idEmisor)
+    {
+        return await _context.Dislikes
+            .Include(d => d.Emisor)
+            .Include(d => d.Receptor)
+            .Where(d => d.IdEmisor == idEmisor)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Dislike>> ObtenerPorReceptorAsync(int idReceptor)
+    {
+        return await _context.Dislikes
+            .Include(d => d.Emisor)
+            .Include(d => d.Receptor)
+            .Where(d => d.IdReceptor == idReceptor)
+            .ToListAsync();
+    }
+
+    public async Task EliminarAsync(int id)
+    {
+        var dislike = await _context.Dislikes.FindAsync(id);
+        if (dislike != null)
+        {
+            _context.Dislikes.Remove(dislike);
+            await _context.SaveChangesAsync();
+        }
+    }
 }
